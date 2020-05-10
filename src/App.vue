942a3b799a51b91ea22ec7a18853d26ec53f5b90
cnotv <contact@cnotv.xyz>
2020-05-10 18:43:21 +0200

    Remove navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Weather map</router-link> |
-      <router-link to="/login">Login</router-link>
-    </div>
     <router-view />
   </div>
 </template>
