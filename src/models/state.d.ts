12901ffbff095f6be5b45a23c8b2e9d4b0105b18
cnotv <contact@cnotv.xyz>
2020-05-10 14:54:05 +0200

    Add State model and basic configuration

---
+++ src/models/state.d.ts
@@ -0,0 +1,7 @@
+type Coords = [string, string];
+
+interface State {
+  apiKey: string;
+  coords: Coords
+}
+
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,33 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    apiKey: "",
+    coords: ['', ''] as Coords,
+  },
+  mutations: {
+    SET_COORDS: (state: State, coords: Coords) => {
+      state.coords = coords
+    },
+    SET_API_KEY: (state: State, apiKey: string) => {
+      state.apiKey = apiKey
+    },
+  },
+  actions: {
+    setCoords({ commit }, coords: Coords) {
+      commit("SET_COORDS", coords)
+    },
+    setApiKey({ commit }, apiKey: string) {
+      commit("SET_API_KEY", apiKey)
+    }
+  },
+  getters: {
+    getApiKey: (state) => {
+      return state.apiKey;
+    },
+    getCoords: (state) => {
+      return state.coords;
+    },
+  },
+  modules: {},
 });
