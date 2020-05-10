695afd6c0231eb209477d5cc8a2b968ed6f5b39a
cnotv <contact@cnotv.xyz>
2020-05-10 17:38:17 +0200

    Add OWM response

---
+++ src/models/owm.d.ts
@@ -0,0 +1,58 @@
+interface Owm {
+  coord:      OwmCoord;
+  weather:    OwmWeather[];
+  base:       string;
+  main:       OwmMain;
+  visibility: number;
+  wind:       OwmWind;
+  rain:       OwmRain;
+  clouds:     OwmClouds;
+  dt:         number;
+  sys:        OwmSys;
+  timezone:   number;
+  id:         number;
+  name:       string;
+  cod:        number;
+}
+
+interface OwmClouds {
+  all: number;
+}
+
+interface OwmCoord {
+  lon: number;
+  lat: number;
+}
+
+interface OwmMain {
+  temp:       number;
+  feels_like: number;
+  temp_min:   number;
+  temp_max:   number;
+  pressure:   number;
+  humidity:   number;
+}
+
+interface OwmRain {
+  "1h": number;
+}
+
+interface OwmSys {
+  type:    number;
+  id:      number;
+  country: string;
+  sunrise: number;
+  sunset:  number;
+}
+
+interface OwmWeather {
+  id:          number;
+  main:        string;
+  description: string;
+  icon:        string;
+}
+
+interface OwmWind {
+  speed: number;
+  deg:   number;
+}
