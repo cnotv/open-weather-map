85c4ab90c373f31358355c15773dad5cb4be0fe8
cnotv <contact@cnotv.xyz>
2020-05-10 15:30:54 +0200

    Allow to set API keys in Login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,5 +2,24 @@
   <div>
     <h1>Login</h1>
     <p>An API key is required to display the map.</p>
+    <input type="text" v-model="apiKey" />
+    <button v-on:click="setApiKey(apiKey)">Add key</button>
   </div>
 </template>
+
+
+<script lang="ts">
+import Vue from "vue";
+import { mapActions } from "vuex";
+
+export default Vue.extend({
+  data() {
+    return {
+      apiKey: ""
+    };
+  },
+  methods: {
+    ...mapActions(["setApiKey"])
+  }
+});
+</script>
