f65f0002b798a75bba6071e9af48d6b5ce474287
cnotv <contact@cnotv.xyz>
2020-05-10 18:44:06 +0200

    Review all the requests and state management according to the new models

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,35 +1,81 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { Store } from "vuex";
+import api from "@/helpers/api";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     apiKey: "",
-    coords: ['', ''] as Coords,
+    data: {},
   },
+
   mutations: {
-    SET_COORDS: (state: State, coords: Coords) => {
-      state.coords = coords
+    SET_LOCATION: (state: State, location: MapLocation) => {
+      state.location = location;
+    },
+    SET_COORDS: (state: State, coords: OwmCoord) => {
+      state.coords = coords;
     },
     SET_API_KEY: (state: State, apiKey: string) => {
-      state.apiKey = apiKey
+      state.apiKey = apiKey;
+    },
+    SET_CACHE: (state: State, payload: { location: string; data: Owm }) => {
+      state.data[payload.location] = payload.data;
+    },
+    SET_MAP: (state: State, map: Owm) => {
+      state.map = map;
     },
   },
+
   actions: {
-    setCoords({ commit }, coords: Coords) {
-      commit("SET_COORDS", coords)
-    },
     setApiKey({ commit }, apiKey: string) {
-      commit("SET_API_KEY", apiKey)
-    }
+      commit("SET_API_KEY", apiKey);
+    },
+    async setLocation({ commit, state }, location: MapLocation) {
+      commit("SET_LOCATION", location);
+      if (!state.apiKey) return;
+
+      const { city, country } = location;
+      const q = `${city},${country}`;
+      const cache = state.data[q];
+
+      if (cache) {
+        commit("SET_MAP", cache);
+        commit("SET_COORDS", cache.coord);
+      } else {
+        const params: ApiParams = {
+          appid: state.apiKey,
+          q,
+        };
+        const mapData = await api.fetchMap(params);
+        commit("SET_MAP", mapData);
+        commit("SET_COORDS", mapData.coord);
+        commit("SET_CACHE", { location: location, data: mapData });
+      }
+    },
+    async setCoords({ commit, state }, coords: OwmCoord) {
+      commit("SET_COORDS", coords);
+      if (!state.apiKey) return;
+
+      const params: ApiParams = {
+        appid: state.apiKey,
+        ...coords,
+      };
+      const mapData = await api.fetchMap(params);
+      commit("SET_MAP", mapData);
+    },
   },
+
   getters: {
-    getApiKey: (state) => {
+    getApiKey: (state: State) => {
       return state.apiKey;
     },
-    getCoords: (state) => {
+    getCoords: (state: State) => {
       return state.coords;
+    },
+    getMap: (state: State) => {
+      return state.map;
     },
   },
   modules: {},
