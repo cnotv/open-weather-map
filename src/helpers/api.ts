fd0b35884fd03e28d8e491b821cff88ab07fb0d6
Giuseppe Leo <giuseppe.leo@retresco.de>
2020-05-19 12:09:00 +0200

    Remove absolute protocoll

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -4,7 +4,7 @@ import { AxiosRequestConfig } from "axios";
 export default {
   async fetchMap(params: ApiParams): Promise<Owm> {
     params.units = 'metric';
-    const url = `http://api.openweathermap.org/data/2.5/weather`;
+    const url = `//api.openweathermap.org/data/2.5/weather`;
     const config: AxiosRequestConfig = { params };
 
     return axios
